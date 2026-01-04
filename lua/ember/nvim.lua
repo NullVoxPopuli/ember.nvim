b5232aecd89449ac845fb565505420918bf33207
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-03 19:28:17 -0500

    Try a TSUpdate autocmd

--- lua/ember/nvim.lua
+++ lua/ember/nvim.lua
@@ -6,22 +6,28 @@
 local M = {}
 
 function M.config()
-  -- no-ops if any of these languages are already installed
-  require 'nvim-treesitter'.install {
-    -- Web Languages
-    "javascript", "typescript",
-    "html", "css", "regex",
-    -- Web Framework Languages
-    "glimmer", "glimmer_javascript", "glimmer_typescript",
-    -- Documentation Languages
-    "markdown", "markdown_inline",
-    -- "help", -- missing?
-    -- "comment", -- slow?
-    "jsdoc",
-  }
 end
 
 function M.setup()
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'TSUpdate',
+    callback = function()
+      -- no-ops if any of these languages are already installed
+      require 'nvim-treesitter'.install {
+        -- Web Languages
+        "javascript", "typescript",
+        "html", "css", "regex",
+        -- Web Framework Languages
+        "glimmer", "glimmer_javascript", "glimmer_typescript",
+        -- Documentation Languages
+        "markdown", "markdown_inline",
+        -- "help", -- missing?
+        -- "comment", -- slow?
+        "jsdoc",
+      }
+    end
+  })
+
   -- Needed for markdown highlighting
   vim.treesitter.language.register('glimmer_javascript', 'gjs')
   vim.treesitter.language.register('glimmer_typescript', 'gts')
