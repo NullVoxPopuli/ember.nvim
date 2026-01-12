60b9db08de0553ebaf349f17187ac8f6116db6cc
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-12 15:14:58 -0500

    Update glimmer queries

--- queries/glimmer/injections.scm
+++ queries/glimmer/injections.scm
@@ -3,19 +3,11 @@
   (#set! injection.language "comment"))
 
 ; <style> tags
-((element_node
-  (element_node_start
-    (tag_name) @_tag_name
-    (#eq? @_tag_name "style"))) @injection.content
-  (#offset! @injection.content 0 7 0 -8)
-  (#set! injection.language "css")
-  (#set! injection.include-children))
+((style_element
+  (raw_text) @injection.content)
+ (#set! injection.language "css"))
 
 ; <script> tags
-((element_node
-  (element_node_start
-    (tag_name) @_tag_name
-    (#eq? @_tag_name "script"))) @injection.content
-  (#offset! @injection.content 0 8 0 -9)
-  (#set! injection.language "glimmer_javascript")
-  (#set! injection.include-children))
+((script_element
+  (raw_text) @injection.content)
+ (#set! injection.language "javascript"))
